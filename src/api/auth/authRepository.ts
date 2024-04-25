2cef8f4a9ec30b7dc76b025e5467094bb4e628ae
FelipeESchmidt <schmidtfe2@gmail.com>
2024-04-25 00:43:38 -0300

    feat: using users controller functions to validate login and register new user

--- src/api/auth/authRepository.ts
+++ src/api/auth/authRepository.ts
@@ -1,13 +1,15 @@
+import { checkUserByEmail, checkUserByEmailAndPassword, createUser } from '@/database/controllers';
+
 import { Auth, Register } from './authModel';
 
 export const authRepository = {
   loginAsync: async (auth: Auth): Promise<boolean> => {
-    return auth.email === 'user@user.com' && auth.password === 'password';
+    return await checkUserByEmailAndPassword(auth.email, auth.password);
   },
   verifyEmailAlreadyExistsAsync: async (register: Register): Promise<boolean> => {
-    return register.email === 'user@user.com';
+    return await checkUserByEmail(register.email);
   },
-  registerAsync: async (auth: Auth): Promise<boolean> => {
-    return true;
+  registerAsync: async (register: Register): Promise<boolean> => {
+    return await createUser(register.name, register.email, register.password);
   },
 };
