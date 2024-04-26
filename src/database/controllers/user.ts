15b22a807940a52894226290d178fcbfeee6bfe5
FelipeESchmidt <schmidtfe2@gmail.com>
2024-04-25 23:52:08 -0300

    feat: creating updateUser query string and function and adapting queries to use tables enum

--- src/database/controllers/user.ts
+++ src/database/controllers/user.ts
@@ -1,34 +1,46 @@
-import { executeInsertQuery, executeQuery } from '@/database/queries';
+import { executeInsertQuery, executeQuery, executeUpdateQuery } from '@/database/queries';
+import { ETableNames } from '@/database/tables';
 
 export const checkUserByEmail = async (email: string): Promise<boolean> => {
   try {
-    const query = 'SELECT COUNT(*) as count FROM users WHERE email = ?';
+    const query = `SELECT COUNT(*) as count FROM ${ETableNames.USERS} WHERE email = ?`;
     const [result] = await executeQuery<{ count: number }>(query, [email]);
     return result.count > 0;
   } catch (error) {
-    console.error('Error validating user email: ', error);
+    console.error(`Error validating user email: `, error);
     throw error;
   }
 };
 
 export const checkUserByEmailAndPassword = async (email: string, password: string): Promise<boolean> => {
   try {
-    const query = 'SELECT COUNT(*) as count FROM users WHERE email = ? AND password = ?';
+    const query = `SELECT COUNT(*) as count FROM ${ETableNames.USERS} WHERE email = ? AND password = ?`;
     const [result] = await executeQuery<{ count: number }>(query, [email, password]);
     return result.count > 0;
   } catch (error) {
-    console.error('Error validating user email and password: ', error);
+    console.error(`Error validating user email and password: `, error);
     throw error;
   }
 };
 
 export const createUser = async (name: string, email: string, password: string): Promise<boolean> => {
   try {
-    const query = 'INSERT INTO users (name, email, password) VALUES (?, ?, ?)';
+    const query = `INSERT INTO ${ETableNames.USERS} (name, email, password) VALUES (?, ?, ?)`;
     await executeInsertQuery(query, [name, email, password]);
     return true;
   } catch (error) {
-    console.error('Error creating user: ', error);
+    console.error(`Error creating user: `, error);
+    throw error;
+  }
+};
+
+export const updateUser = async (name: string, email: string, password: string): Promise<boolean> => {
+  try {
+    const query = `UPDATE ${ETableNames.USERS} SET name = ?, password = ? WHERE email = ?`;
+    await executeUpdateQuery(query, [name, password, email]);
+    return true;
+  } catch (error) {
+    console.error(`Error updating user: `, error);
     throw error;
   }
 };
