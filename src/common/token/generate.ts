881eb5b586289b089e9bbb62aff9b8e857c1be31
FelipeESchmidt <schmidtfe2@gmail.com>
2024-04-25 23:49:37 -0300

    feat: changing token expiresIn from 1m to 1d

--- src/common/token/generate.ts
+++ src/common/token/generate.ts
@@ -1,8 +1,9 @@
 import jsonwebtoken from 'jsonwebtoken';
 
 import { env } from '@/common/utils/envConfig';
+
 import { TokenInfo } from './types';
 
 export const generateToken = (info: TokenInfo): string => {
-  return jsonwebtoken.sign(info, env.JWT_SECRET, { expiresIn: '1m' });
+  return jsonwebtoken.sign(info, env.JWT_SECRET, { expiresIn: '1d' });
 };
