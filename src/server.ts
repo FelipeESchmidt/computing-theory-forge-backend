4e8ae0844fd51901454784139beed445391bbb90
FelipeESchmidt <schmidtfe2@gmail.com>
2024-05-01 14:26:57 -0300

    chore: enabling all cors origins

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,17 @@
+import bodyParser from 'body-parser';
 import cors from 'cors';
 import express, { Express } from 'express';
-import bodyParser from 'body-parser';
 import helmet from 'helmet';
 import { pino } from 'pino';
 
 import { healthCheckRouter } from '@/api/healthCheck/healthCheckRouter';
-import { authRouter } from './api/auth/authRouter';
 import { openAPIRouter } from '@/api-docs/openAPIRouter';
 import errorHandler from '@/common/middleware/errorHandler';
 import rateLimiter from '@/common/middleware/rateLimiter';
 import requestLogger from '@/common/middleware/requestLogger';
 import { env } from '@/common/utils/envConfig';
+
+import { authRouter } from './api/auth/authRouter';
 
 const logger = pino({ name: 'server start' });
 const app: Express = express();
@@ -19,7 +20,7 @@ const app: Express = express();
 app.set('trust proxy', true);
 
 // Middlewares
-app.use(cors({ origin: env.CORS_ORIGIN, credentials: true }));
+app.use(cors({ origin: env.CORS_ORIGIN, credentials: false }));
 app.use(helmet());
 app.use(rateLimiter);
 app.use(bodyParser.json());
