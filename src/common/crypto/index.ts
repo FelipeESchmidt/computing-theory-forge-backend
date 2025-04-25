0204d5767109fed40266a620e03f910fa19ca792
Felipe E. Schmidt <schmidtfe2@gmail.com>
2025-04-25 00:16:33 -0300

    feat: fixing crypto function

--- src/common/crypto/index.ts
+++ src/common/crypto/index.ts
@@ -3,7 +3,7 @@ import crypto from 'crypto';
 import { env } from '@/common/utils/envConfig';
 
 export const encryptPassword = (password: string): string => {
-  const cipher = crypto.createCipher('aes-256-cbc', env.ENCRYPTION_KEY);
+  const cipher = crypto.createCipheriv('aes-256-cbc', env.ENCRYPTION_KEY, null);
   let encryptedPassword = cipher.update(password, 'utf8', 'hex');
   encryptedPassword += cipher.final('hex');
   return encryptedPassword;
